1bef19b5ac4f0d860f38f3d48a9661254a50ac58
Laura <lauragonzafernan@gmail.com>
2021-01-03 20:46:43 +0100

    level02

---
+++ Gobla_Arcade/Assets/Scripts/Managers/SavePath.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePath : MonoBehaviour
+{
+//Se encarga de gestionar las decisiones tomadas por el jugador que luego llevaran a un final y otro
+}
